3bde3b696c5716261d673d79a756df15eaf07814
hsmtkk <hsmtkk@gmail.com>
2023-10-20 17:07:50 +0900

    hash password

--- repo/user.go
+++ repo/user.go
@@ -2,6 +2,7 @@ package repo
 
 import (
 	"context"
+	"crypto/sha256"
 	"fmt"
 	"time"
 
@@ -21,8 +22,9 @@ func NewUser(conn *pgx.Conn) *User {
 func (u *User) NewUser(ctx context.Context, name string, email string, password string) (model.User, error) {
 	result := model.User{}
 	uuid := uuid.New().String()
+	hashedPassword := sha256Hash(password)
 	createdAt := time.Now()
-	if err := u.conn.QueryRow(ctx, `INSERT INTO users (uuid, name, email, password, created_at) values ($1, $2, $3, $4, $5) RETURNING id`, uuid, name, email, password, createdAt).Scan(&result.ID); err != nil {
+	if err := u.conn.QueryRow(ctx, `INSERT INTO users (uuid, name, email, password, created_at) values ($1, $2, $3, $4, $5) RETURNING id`, uuid, name, email, hashedPassword, createdAt).Scan(&result.ID); err != nil {
 		return result, fmt.Errorf("insert failed: %w", err)
 	}
 	result.UUID = uuid
@@ -31,4 +33,10 @@ func (u *User) NewUser(ctx context.Context, name string, email string, password 
 	result.Password = password
 	result.CreatedAt = createdAt
 	return result, nil
+}
+
+func sha256Hash(plainText string) string {
+	sum := sha256.Sum256([]byte(plainText))
+	hashed := fmt.Sprintf("%x", sum)
+	return hashed
 }
